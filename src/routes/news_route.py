f5aff7e35de407adeb78ee821c0b70bd42d245c1
Guozao Meng <guozao.m@gmail.com>
2024-10-27 22:07:24 +0400

    Save history

--- src/routes/news_route.py
+++ src/routes/news_route.py
@@ -1,6 +1,9 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, session
 
+from src.constants.enums import SentenceSource
 from src.dao.news_dao import NewsDao
+from src.dao.read_history_dao import ReadHistoryDao
+from src.utils.auth_utils import is_logged_in
 
 bp = Blueprint('news', __name__)
 
@@ -27,5 +30,10 @@ def get_news(id: int):
     if not news:
         return "News not found", 404
 
-    return render_template('news.html', news=news)
+    last_read_sentence_no = 0
+    if is_logged_in():
+        history = ReadHistoryDao.get_one(session["user"]["id"], SentenceSource.NEWS.value, news.id)
+        last_read_sentence_no = history.sentence_no if history else 0
+
+    return render_template('news.html', news=news, last_read_sentence_no=last_read_sentence_no)
 
