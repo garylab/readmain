5d6066326e9b03fe954429f78acfcbdd4927806e
Guozao Meng <guozao.m@gmail.com>
2024-12-24 21:50:19 +0400

    Update start.sh

--- crawl_news.sh
+++ crawl_news.sh
@@ -1,1 +1,2 @@
-docker run --rm --env-file /home/gary/docker_services/readmain/local.env english-web python /workspace/src/crawl_news.py
+docker build --tag english-web . \
+ && docker run --rm --env-file /home/gary/docker_services/readmain/local.env english-web python /workspace/src/crawl_news.py
