d8da9694606f4690e9a68c3b96215cb54d1333a0
Guozao Meng <guozao.m@gmail.com>
2025-05-11 21:29:19 +0400

    echo ROOT_DIR

--- crawl_news.sh
+++ crawl_news.sh
@@ -1,3 +1,5 @@
 export ROOT_DIR="$(dirname $0)"
+echo "You are in dir: $ROOT_DIR"
+
 docker build -f "$ROOT_DIR/Dockerfile" --tag english-web . \
  && docker run --rm --env-file "$ROOT_DIR/local.env" english-web python /workspace/src/crawl_news.py
